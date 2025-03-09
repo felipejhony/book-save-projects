26f9b4fc5d0b86e726942f4f800605cfe05f662d
felipejhony <felipejhony91@gmail.com>
2025-03-09 15:33:52 -0300

    add book-save-thymeleaf project

---
+++ 05-book-save-thymeleaf/src/main/java/br/com/felipe/booksave/BooksaveApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipe.booksave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksaveApplication.class, args);
+	}
+
+}
---
+++ 05-book-save-thymeleaf/src/test/java/br/com/felipe/booksave/BooksaveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipe.booksave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
